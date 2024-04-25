5356cef3bdc1828e6c671f3a4e365092f5816529
Akshay Joshi <akshayjoshi1117@gmail.com>
2024-04-25 23:11:31 +0530

    added handler

---
+++ handler/order.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Order struct{}
+
+func (o *Order) Create(w http.ResponseWriter, r *http.Request){
+	fmt.Println("create order")
+}
+
+func (o *Order) List(w http.ResponseWriter, r *http.Request){
+	fmt.Println("List order")
+}
+
+func (o *Order) GetById(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Get order")
+}
+
+func (o *Order) UpdateById(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Update order")
+}
+
+func (o *Order) DeletetById(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Delete order")
+}
+
